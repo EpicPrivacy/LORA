367b7edda0a637b039fc921989306bfb9f057e55
EpicPrivacy <47374573+EpicPrivacy@users.noreply.github.com>
2023-10-27 13:57:57 +0800

    update

--- app/src/main/java/com/example/lorav21/sms_verification.java
+++ app/src/main/java/com/example/lorav21/sms_verification.java
@@ -2,13 +2,26 @@ package com.example.lorav21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sms_verification extends AppCompatActivity {
 
+    Button btn_verify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms_verification);
+
+        btn_verify = findViewById(R.id.btn_verify);
+
+        btn_verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(sms_verification.this, MainActivity.class));
+            }
+        });
     }
 }
