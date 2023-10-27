702e3408a00f7f4d381201643cb032a1e17825a6
EpicPrivacy <47374573+EpicPrivacy@users.noreply.github.com>
2023-10-27 13:44:18 +0800

    a

--- app/src/main/java/com/example/lorav21/register_form.java
+++ app/src/main/java/com/example/lorav21/register_form.java
@@ -2,13 +2,26 @@ package com.example.lorav21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register_form extends AppCompatActivity {
 
+    Button btn_submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form);
+
+        btn_submit = findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(register_form.this, sms_verification.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lorav21/shop_contact_details.java
@@ -0,0 +1,14 @@
+package com.example.lorav21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shop_contact_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_contact_details);
+    }
+}
